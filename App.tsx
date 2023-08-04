8fae59c18e0111e82cd2a86c02a491f350f135ed
nandobfer <nandobfer@yahoo.com.br>
2023-08-04 17:31:56 -0300

    init: working app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { useEffect, useRef, useState } from "react"
+import { WebView } from "react-native-webview"
+import { Camera } from "expo-camera"
+import { BackHandler } from "react-native"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const webViewRef = useRef(null)
+    const [hasPermission, setHasPermission] = useState<boolean | null>(null)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    useEffect(() => {
+        ;(async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync()
+            setHasPermission(status === "granted")
+        })()
+    }, [])
+    useEffect(() => {
+        const backAction = () => {
+            // @ts-ignore
+            webViewRef.current?.goBack()
+            return true
+        }
+
+        const backHandler = BackHandler.addEventListener("hardwareBackPress", backAction)
+
+        return () => backHandler.remove()
+    }, [])
+
+    return (
+        <>
+            <StatusBar style="auto" />
+            <WebView
+                ref={webViewRef}
+                source={{ uri: "https://app.mirasuprimentos.com.br" }}
+                style={{ flex: 1 }}
+                allowFileAccess
+                mediaCapturePermissionGrantType="grant"
+                mediaPlaybackRequiresUserAction={false}
+            />
+        </>
+    )
+}
